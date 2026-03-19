30e9d92a08d8732e25dc6503886257e3b5e1af9f
Boris Mann <boris@bmannconsulting.com>
2026-03-18 19:33:28 -0700

    Update FEED_URI and FEED_URL for new identifiers
    Closes #57 - thanks Nick

--- src/lib/bsky.ts
+++ src/lib/bsky.ts
@@ -7,10 +7,10 @@ const BSKY_PUBLIC_API = "https://public.api.bsky.app";
 const PAGE_SIZE = 20;
 
 export const FEED_URI =
-  "at://did:plc:cbkjy5n7bk3ax2wplmtjofq2/app.bsky.feed.generator/AtmosphereConf";
+  "at://did:plc:3xewinw4wtimo2lqfy5fm5sw/app.bsky.feed.generator/atmosphereconf";
 
 export const FEED_URL =
-  "https://bsky.app/profile/did:plc:cbkjy5n7bk3ax2wplmtjofq2/feed/AtmosphereConf";
+  "https://bsky.app/profile/atmosphereconf.org/feed/atmosphereconf;
 
 // --- Types ---
 
